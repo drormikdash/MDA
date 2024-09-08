4f0cb5624340af083be60d4b91d27a8b95b2aeb6
drormikdash <101096999+drormikdash@users.noreply.github.com>
2024-09-08 18:23:20 +0300

    Update wrapcells.py
    with Daniel Goldz suggestions

--- wrapcells.py
+++ wrapcells.py
@@ -12,16 +12,4 @@ df = pd.read_excel(file_name, header=None)
 
 # Get the first column (assuming it is labeled 'A' or is the first column)
 first_column = df.iloc[:, 0]
-#print(first_column)
-maxrow = first_column.shape
-
-#print(maxrow, maxrow[0])
-currentRow=1
-print(f'(', end=" ")
-# Iterate over the cells in the first column and print them
-for cell in first_column:
-    if  (currentRow<maxrow[0]):
-        print(f'"{cell}",' , end=" ")
-    else:
-        print(f'"{cell}")', end=" ")
-    currentRow+=1
+print("(", ','.join([f'{i}'  for i in first_column]), ")")
