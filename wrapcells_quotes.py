5b043267d2bf7684d1bf07035b81dd656e2ef0f4
drormikdash <101096999+drormikdash@users.noreply.github.com>
2024-09-09 18:21:47 +0300

    Add files via upload
    script that also surrounds each value with quotes

---
+++ wrapcells_quotes.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sys
+
+# Ask the user for the Excel file name
+#file_name = input("Please enter the name of the Excel file: ")
+
+#print(sys.argv)
+
+file_name = sys.argv[1]
+# Load the Excel file
+df = pd.read_excel(file_name, header=None)
+
+# Get the first column (assuming it is labeled 'A' or is the first column)
+first_column = df.iloc[:, 0]
+print("(", ','.join([f'"{i}"'  for i in first_column]), ")")
